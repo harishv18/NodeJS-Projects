4544c1ed600ed7b7547eb600b859826fd6372b00
Harish V <harish00411@gmail.com>
2023-11-23 02:02:04 +0530

    QR Generator Project

---
+++ QrGenerator/index.js
@@ -0,0 +1,20 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+inquirer
+  .prompt([
+    {"message": "Type in your URL", "name": "URL"}
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_png = qr.image(url);
+    qr_png.pipe(fs.createWriteStream('qr_img.png'))
+    // var png_string = qr.imageSync(url)
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
